143b2a4b292a8ca31b373a39616757e4bacc0088
Tim Li <ltim@uber.com>
2025-02-21 06:19:22 -0800

    Respond to comments

--- common/definition/workflowIdentifier.go
+++ common/definition/workflowIdentifier.go
@@ -20,6 +20,8 @@
 
 package definition
 
+import "github.com/uber/cadence/common"
+
 type (
 	// WorkflowIdentifier is the combinations which represent a workflow
 	WorkflowIdentifier struct {
@@ -34,7 +36,7 @@ func (wi *WorkflowIdentifier) Size() uint64 {
 	// Calculate the size of strings in bytes, we assume that all those fields are using ASCII which is 1 byte per char
 	size := len(wi.DomainID) + len(wi.WorkflowID) + len(wi.RunID)
 	// Each string internally holds a reference pointer and a length, which are 8 bytes each
-	stringOverhead := 3 * 16
+	stringOverhead := 3 * common.StringSizeOverheadBytes
 	return uint64(size + stringOverhead)
 }
 
